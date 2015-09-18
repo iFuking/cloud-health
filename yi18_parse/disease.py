48372735b655f8cc2121b772bd03c60cc8288646
chmwang <chmwang@zju.edu.cn>
2015-09-18 15:29:47 +0800

    处理～/yi18/disease/下所有文件，包括json中文本内容的提取、对文本内容中文分词、TF-IDF算法生成描述文本内容的特征向量

---
+++ yi18_parse/disease.py
@@ -0,0 +1,63 @@
+import json
+from bs4 import BeautifulSoup
+import jieba
+import jieba.analyse
+
+
+def parse_content(d):
+    for k in d.keys():
+        if isinstance(d[k], dict):
+            parse_content(d[k])
+        else:
+            dct_filter[k] = d[k]
+    return
+
+
+EMPTY_FILE_NUM = 100
+req_id = 0
+missed = 0
+
+log_path = '/home/dick/filter/disease/0_log.txt'
+f_log = open(log_path, 'w')
+
+while True:
+    if missed > EMPTY_FILE_NUM:
+        break
+    req_id += 1
+    read_path = '/home/dick/yi18/disease/%d.txt' % req_id
+    try:
+        f = open(read_path, 'r')
+    except IOError as e:
+        missed += 1
+        print e
+        continue
+
+    missed = 0
+    data = f.read()
+    f.close()
+    dct = json.loads(data)
+    dct_filter = {}
+    parse_content(dct)
+    content = ""
+    for key in dct_filter.keys():
+        if isinstance(dct_filter[key], int) or isinstance(dct_filter[key], bool):
+            dct_filter[key] = str(dct_filter[key])
+        content += str(dct_filter[key].encode('utf8'))
+
+    soup = BeautifulSoup(content)
+    content = soup.getText().encode('utf8')
+    write_path = '/home/dick/filter/disease/%d.txt' % req_id
+    try:
+        f = open(write_path, 'w')
+    except IOError as e:
+        print e
+        continue
+    f.write(content)
+    f.close()
+    f_log.write('%d.txt: ' % req_id)
+    tags = jieba.analyse.extract_tags(content, 5, True)
+    for word, weight in tags:
+        f_log.write('%s(%f), ' % (word.encode('utf8'), weight))
+    f_log.write('\n')
+    print "We're done with %s" % write_path
+f_log.close()
