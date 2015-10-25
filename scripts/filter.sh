e2eb56b784431b85b8f3cf663631feba89cf61e0
chmwang <chmwang@zju.edu.cn>
2015-10-25 11:19:43 +0800

    yi18 database built & release shell for recommendation, data structure changed (from file system to mysql database)

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd /home/dick/filter/
+
+FILTER_PATH='/home/dick/filter/'
+cd ${FILTER_PATH}
 rm -r *
 mkdir disease && mkdir drug && mkdir lore && mkdir news && mkdir symptom
 
-cd /home/dick/PycharmProjects/pyProject/yi18_parse/
+RUN_PATH='/home/dick/PycharmProjects/pyProject/yi18_parse/'
+cd ${RUN_PATH}
 python drug.py && python general.py
---
+++ scripts/yi18_database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RUN_PATH='/home/dick/PycharmProjects/pyProject/build_database_yi18/'
+cd ${RUN_PATH}
+python ask.py
+python book.py
+python check.py
+python cook.py
+python disease.py
+python drug.py
+python food.py
+python lore.py
+python news.py
+python surgery.py
+python symptom.py
