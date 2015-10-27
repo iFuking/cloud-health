711eb5e6718de2dcc42901c6efb1e532c839e2be
chmwang <chmwang@zju.edu.cn>
2015-10-27 19:19:20 +0800

    finished filter part (general.py)

--- build_database_filter/general.py
+++ build_database_filter/general.py
@@ -3,6 +3,7 @@ import json
 from bs4 import BeautifulSoup
 import jieba
 import jieba.analyse
+import logging
 
 # yi18 database & its cursor, encoding: utf8
 db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='yi18')
@@ -14,54 +15,87 @@ db_filter = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='
 cur_filter = db_filter.cursor()
 db_filter.set_character_set('utf8')
 
+logging.basicConfig(level=logging.INFO)
+
 # database tables
 table_name = [
     'ask', 'book', 'checks', 'disease', 'drug',
     'food', 'lore', 'news', 'surgery', 'symptom'
 ]
 
-SQL = 'SELECT * FROM ask'
-cursor.execute(SQL)
-results = cursor.fetchall()
-
-dct_value = {}
+# in different tables, column name differ
+name = [
+    'title', 'name', 'name', 'name', 'name',
+    'name', 'title', 'title', 'name', 'name'
+]
+class_name = [
+    'classname', 'bookclass', 'menu', 'department', 'category'
+    'menu', 'classname', 'tag', 'department', 'place'
+]
 
 
 # parse json format function, key-value
 def json_value(d):
     for k in d.keys():
+        # type(d[k]) is also dict, call function recursively
         if isinstance(d[k], dict):
             json_value(d[k])
+        # type(d[k]) is list, parse each node
         elif isinstance(d[k], list):
             for it in d[k]:
                 json_value(it)
+        # until key-value structure, save it
         else:
             dct_value[k] = d[k]
     return
 
 
-for record in results:
+# main function & algorithm logic, build table one by one
+i = -1
+for table in table_name:
+    i += 1
+    # fetch a table's records
+    SQL = 'SELECT * FROM %s' % table
+    cursor.execute(SQL)
+    results = cursor.fetchall()
 
-    json_value(json.loads(record[3]))
-    content = ''
-    for key in dct_value.keys():
-        if isinstance(dct_value[key], unicode):
-            content += dct_value[key]
+    for record in results:
+        dct_value = {}
+        # call json_value() function, parse json(content) & save pair<key, value>
+        # filter the first time
+        json_value(json.loads(record[3]))
+        content = ''
+        for key in dct_value.keys():
+            # only unicode(string) included, int/long/bool.. ignored
+            # filter the second time
+            if isinstance(dct_value[key], unicode):
+                content += dct_value[key]
 
-    soup = BeautifulSoup(content)
-    content = soup.getText().replace('\n', '')
-    tags = jieba.analyse.extract_tags(content, 5, True)
-    keywords = weight = ''
-    for word, w in tags:
-        keywords += (word+',')
-        weight += (str(w)+',')
+        soup = BeautifulSoup(content)
+        # beautiful soup get text (tag filter) & newline filter
+        # filter the third time
+        content = soup.getText().replace('\n', '')
 
-    SQL = 'INSERT INTO ask(id, title, classname, keywords, weight) VALUES(%s, %s, %s, %s, %s)'
-    cur_filter.execute(SQL, (record[0], record[1], record[2], keywords, weight))
-    db_filter.commit()
+        # jieba extract tags, return top5 key words & its weight
+        tags = jieba.analyse.extract_tags(content, 5, True)
+        keywords = weight = ''
+        # words and weight separated by ','
+        for word, w in tags:
+            keywords += (word+',')
+            weight += (str(w)+',')
 
-    SQL = 'INSERT INTO ask_cache(id, content) VALUES(%s, %s)'
-    cur_filter.execute(SQL, (record[0], content))
-    db_filter.commit()
+        try:
+            SQL = 'INSERT INTO %s(id, %s, %s, keywords, weight) ' % (table, name[i], class_name[i]) + \
+                  'VALUES(%s, %s, %s, %s, %s)'
+            cur_filter.execute(SQL, (record[0], record[1], record[2], keywords, weight))
+            db_filter.commit()
 
-    print 'Finish %d.' % record[0]
+            SQL = 'INSERT INTO %s_cache(id, content) ' % table + 'VALUES(%s, %s)'
+            cur_filter.execute(SQL, (record[0], content))
+            db_filter.commit()
+
+            logging.info("Table %s with id=%s finished." % (table, record[0]))
+
+        except MySQLdb.IntegrityError as e:
+            logging.error('In table `%s` with id=%s, %s.' % (table, record[0], e))
+            continue
