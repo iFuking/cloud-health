1c6c2ddfbc937c6f99d5260c95be2179f2c74768
chmwang <chmwang@zju.edu.cn>
2015-10-28 22:00:12 +0800

    ignore keys & replace '\r\t'

--- build_database_filter/general.py
+++ build_database_filter/general.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import jieba
 import jieba.analyse
 import logging
+import time
 
 # yi18 database & its cursor, encoding: utf8
 db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='yi18')
@@ -50,7 +51,11 @@ def json_value(d):
     return
 
 
-ignore_key = ['img', 'author', 'time']
+ignore_key = [
+    'img', 'from', 'author', 'count',
+    'image', 'ANumber', 'PType', 'factory',
+    'time'
+]
 
 
 # ignore some specify keys
@@ -61,8 +66,9 @@ def ignore_tag(tag):
     return True
 
 
-# main function & algorithm logic, build table one by one
 i = -1
+start_time = time.time()  # timestamp start time
+# main function & algorithm logic, build table one by one
 for table in table_name:
     i += 1
     # fetch a table's records
@@ -85,7 +91,7 @@ for table in table_name:
         soup = BeautifulSoup(content)
         # beautiful soup get text (tag filter) & newline filter
         # filter the third time
-        content = soup.getText().replace('\n', '')
+        content = soup.getText().replace('\n', '').replace('\r', '').replace('\t', '')
 
         # jieba extract tags, return top5 key words & its weight
         tags = jieba.analyse.extract_tags(content, 5, True)
@@ -96,17 +102,21 @@ for table in table_name:
             weight += (str(w)+',')
 
         try:
+            SQL = 'INSERT INTO %s_cache(id, content) ' % table + 'VALUES(%s, %s)'
+            cur_filter.execute(SQL, (record[0], content))
+            db_filter.commit()
+
             SQL = 'INSERT INTO %s(id, %s, %s, keywords, weight) ' % (table, name[i], class_name[i]) + \
                   'VALUES(%s, %s, %s, %s, %s)'
             cur_filter.execute(SQL, (record[0], record[1], record[2], keywords, weight))
             db_filter.commit()
 
-            SQL = 'INSERT INTO %s_cache(id, content) ' % table + 'VALUES(%s, %s)'
-            cur_filter.execute(SQL, (record[0], content))
-            db_filter.commit()
-
-            logging.info("Table %s with id=%s finished." % (table, record[0]))
+            logging.info(time.strftime('%Y/%m/%d %H:%M:%S--') + 'Table %s with id=%s finished.' % (table, record[0]))
 
         except MySQLdb.IntegrityError as e:
-            logging.error('In table `%s` with id=%s, %s.' % (table, record[0], e))
+            logging.error(time.strftime('%Y/%m/%d %H:%M:%S--') + 'In table `%s` with id=%s, %s.' %
+                          (table, record[0], e))
             continue
+
+    time_now = time.time()
+    logging.info('Total time cost: %ss.' % (time_now-start_time))
