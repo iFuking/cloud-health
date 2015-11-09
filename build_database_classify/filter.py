0db0ef13a6085e3070ff8d0dc7da2158088c2296
chmwang <chmwang@zju.edu.cn>
2015-11-09 18:05:34 +0800

    build filter database, code refactoring & parse json format & tf-idf

--- build_database_classify/filter.py
+++ build_database_classify/filter.py
@@ -33,21 +33,22 @@ class_name = [
     'classname', 'bookclass', 'menu', 'department', 'category',
     'menu', 'classname', 'tag', 'department', 'place'
 ]
+dct_value = {}
 
 
 # parse json format function, key-value
-def json_value(d):
-    for k in d.keys():
+def json_value(dct):
+    for key in dct.keys():
         # type(d[k]) is also dict, call function recursively
-        if isinstance(d[k], dict):
-            json_value(d[k])
+        if isinstance(dct[key], dict):
+            json_value(dct[key])
         # type(d[k]) is list, parse each node
-        elif isinstance(d[k], list):
-            for it in d[k]:
+        elif isinstance(dct[key], list):
+            for it in dct[key]:
                 json_value(it)
         # until key-value structure, save it
         else:
-            dct_value[k] = d[k]
+            dct_value[key] = dct[key]
     return
 
 
@@ -66,57 +67,62 @@ def ignore_tag(tag):
     return True
 
 
-i = -1
-start_time = time.time()  # timestamp start time
-# main function & algorithm logic, build table one by one
-for table in table_name:
-    i += 1
-    # fetch a table's records
-    SQL = 'SELECT * FROM %s' % table
-    cursor.execute(SQL)
-    results = cursor.fetchall()
+def main():
+    i = -1
+    start_time = time.time()  # timestamp start time
+    # main function & algorithm logic, build table one by one
+    for table in table_name:
+        i += 1
+        # fetch a table's records
+        sql = 'SELECT * FROM %s' % table
+        cursor.execute(sql)
+        results = cursor.fetchall()
 
-    for record in results:
-        dct_value = {}
-        # call json_value() function, parse json(content) & save pair<key, value>
-        # filter the first time
-        json_value(json.loads(record[3]))
-        content = ''
-        for key in dct_value.keys():
-            # only unicode(string) included, int/long/bool.. ignored
-            # filter the second time
-            if ignore_tag(key) and isinstance(dct_value[key], unicode):
-                content += dct_value[key]
+        for record in results:
+            # call json_value() function, parse json(content) & save pair<key, value>
+            # filter the first time
+            json_value(json.loads(record[3]))
+            content = ''
+            for key in dct_value.keys():
+                # only unicode(string) included, int/long/bool.. ignored
+                # filter the second time
+                if ignore_tag(key) and isinstance(dct_value[key], unicode):
+                    content += dct_value[key]
 
-        soup = BeautifulSoup(content)
-        # beautiful soup get text (tag filter) & newline filter
-        # filter the third time
-        content = soup.getText().replace('\n', '').replace('\r', '').replace('\t', '')
+            soup = BeautifulSoup(content)
+            # beautiful soup get text (tag filter) & newline filter
+            # filter the third time
+            content = soup.getText().replace('\n', '').replace('\r', '').replace('\t', '')
 
-        # jieba extract tags, return top5 key words & its weight
-        tags = jieba.analyse.extract_tags(content, 5, True)
-        keywords = weight = ''
-        # words and weight separated by ','
-        for word, w in tags:
-            keywords += (word+',')
-            weight += (str(w)+',')
+            # jieba extract tags, return top5 key words & its weight
+            tags = jieba.analyse.extract_tags(content, 5, True)
+            keywords = weight = ''
+            # words and weight separated by ','
+            for word, w in tags:
+                keywords += (word+',')
+                weight += (str(w)+',')
 
-        try:
-            SQL = 'INSERT INTO %s_cache(id, content) ' % table + 'VALUES(%s, %s)'
-            cur_filter.execute(SQL, (record[0], content))
-            db_filter.commit()
+            try:
+                sql = 'INSERT INTO %s_cache(id, content) ' % table + 'VALUES(%s, %s)'
+                cur_filter.execute(sql, (record[0], content))
+                db_filter.commit()
 
-            SQL = 'INSERT INTO %s(id, %s, %s, keywords, weight) ' % (table, name[i], class_name[i]) + \
-                  'VALUES(%s, %s, %s, %s, %s)'
-            cur_filter.execute(SQL, (record[0], record[1], record[2], keywords, weight))
-            db_filter.commit()
+                sql = 'INSERT INTO %s(id, %s, %s, keywords, weight) ' % (table, name[i], class_name[i]) + \
+                      'VALUES(%s, %s, %s, %s, %s)'
+                cur_filter.execute(sql, (record[0], record[1], record[2], keywords, weight))
+                db_filter.commit()
 
-            logging.info(time.strftime('%Y/%m/%d %H:%M:%S--') + 'Table %s with id=%s finished.' % (table, record[0]))
+                logging.info(time.strftime('%Y/%m/%d %H:%M:%S--') + 'Table %s with id=%s finished.' %
+                             (table, record[0]))
 
-        except MySQLdb.IntegrityError as e:
-            logging.error(time.strftime('%Y/%m/%d %H:%M:%S--') + 'In table `%s` with id=%s, %s.' %
-                          (table, record[0], e))
-            continue
+            except MySQLdb.IntegrityError as e:
+                logging.error(time.strftime('%Y/%m/%d %H:%M:%S--') + 'In table `%s` with id=%s, %s.' %
+                              (table, record[0], e))
+                continue
 
-    time_now = time.time()
-    logging.info('Total time cost: %ss.' % (time_now-start_time))
+        time_now = time.time()
+        logging.info('Total time cost: %ss.' % (time_now-start_time))
+
+
+if __name__ == '__main__':
+    main()
