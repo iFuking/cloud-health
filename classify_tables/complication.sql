9ee0325e5d15b28e97443eb2ec402c7670bb64bf
chmwang <chmwang@zju.edu.cn>
2015-11-09 21:29:09 +0800

    build complication manually, in case of `classify` database dropping, `complication` table deleting

--- classify_tables/complication.sql
+++ classify_tables/complication.sql
@@ -1,10 +1,51 @@
-CREATE TABLE `complication` (
-	`id` INT(11) NOT NULL, 
-	`complications` TEXT DEFAULT NULL, 
-	PRIMARY KEY (`id`)
-) CHARACTER SET = 'UTF8';
+-- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: classify
+-- ------------------------------------------------------
+-- Server version	5.5.44-0ubuntu0.14.04.1
 
-/*
-INSERT INTO complication(id, complications) VALUES(1, '3,4,5,6,');
-INSERT INTO complication(id, complications) VALUES(2, '1,7,8,9,10,11,12,');
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `complication`
+--
+
+DROP TABLE IF EXISTS `complication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `complication` (
+  `id` int(11) NOT NULL,
+  `complications` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `complication`
+--
+
+LOCK TABLES `complication` WRITE;
+/*!40000 ALTER TABLE `complication` DISABLE KEYS */;
+INSERT INTO `complication` VALUES (1,'3,4,5,6,'),(2,'1,7,8,9,10,11,12,');
+/*!40000 ALTER TABLE `complication` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-09 19:36:19
