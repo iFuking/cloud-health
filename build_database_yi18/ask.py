f7c6b7b376caec6b66649443031f2ac5a3eaca54
chmwang <chmwang@zju.edu.cn>
2015-11-09 14:53:10 +0800

    build database `yi18`, table `ask` & log information

--- build_database_yi18/ask.py
+++ build_database_yi18/ask.py
@@ -7,6 +7,7 @@ db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='yi18')
 cursor = db.cursor()
 db.set_character_set('utf8')
 
+# database data reading from file system, read path
 home_path = '/home/dick/yi18'
 logging.basicConfig(level=logging.INFO)
 
@@ -16,6 +17,7 @@ EMPTY_RESP_NUM = 100
 def main():
     req_id = empty_resp = 0
     while True:
+        # break if response is empty for continuous certain times
         if empty_resp > EMPTY_RESP_NUM:
             break
         req_id += 1
@@ -26,11 +28,13 @@ def main():
             content = read_file.read()
 
             empty_resp = 0
+            # parse content, json format
             dct = json.loads(content)
             req_id = dct['yi18']['id']
             title = dct['yi18']['title']
             class_name = dct['yi18']['classname']
 
+            # mysql operation
             sql = 'INSERT INTO ask(id, title, classname, content) VALUES(%s, %s, %s, %s)'
             cursor.execute(sql, (req_id, title, class_name, content))
             db.commit()
@@ -38,6 +42,7 @@ def main():
 
         except (IOError, ValueError, TypeError, MySQLdb.IntegrityError) as e:
             empty_resp += 1
+            # error log
             logging.error(e)
             continue
 
