d59f442913a6070d5ec7323ebb9ab686799a329b
chmwang <chmwang@zju.edu.cn>
2015-12-23 17:23:21 +0800

    migrate mysql to mongo

---
+++ mongo_dump/mongo.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+import MySQLdb
+import json
+
+
+db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='filter')
+cursor = db.cursor()
+db.set_character_set('utf8')
+
+
+def mongo_dump_csv():
+    sql = 'SHOW TABLES'
+    cursor.execute(sql)
+    table_name = cursor.fetchall()
+
+    for table in table_name:
+        sql = 'SELECT * FROM %s' % table + \
+              ' INTO OUTFILE \'/tmp/db_csv/%s.csv\'' % table + \
+              ' FIELDS TERMINATED BY \',\'' \
+              ' ENCLOSED BY \'"\'' \
+              ' LINES TERMINATED BY \'\\n\';'
+        print sql
+        cursor.execute(sql)
+        db.commit()
+    return
+
+
+def main():
+    mongo_dump_csv()
+    return
+
+
+if __name__ == '__main__':
+    main()
