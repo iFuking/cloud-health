f0021caf1e8f33dea9155389008a898d069bb18c
chmwang <chmwang@zju.edu.cn>
2016-02-26 18:05:24 +0800

    using wechat official upload media api instead of WechatBasic python sdk, avoiding SSLError (certificate verify failed)

---
+++ image/upload_media.sh
@@ -0,0 +1,1 @@
+curl -F media=@../image/disease.jpg "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=2vpBUkoIgPnPAFqIRsqp5lqZ35u-DC6Y7xoPRRnfvQjO_OWJ2EPCaMoZzBOSg086JC1fx6Z_73Ma6GZZxNLMdl7V00qoAcxmvJ-9042LqLAWYVjACAHJG&type=image"
